2791c5d7b07d1343f0da11ad6568f7e7e6967af8
Jarrett Lusso <jclusso@gmail.com>
2020-03-30 12:44:13 -0400

    Switched remaining function calls to use newer syntax

--- test/account.spec.js
+++ test/account.spec.js
@@ -3,10 +3,10 @@
 const expect = require('chai').expect;
 const blazeverify = require('../lib/blazeverify')('test_7aff7fc0142c65f86a00')
 
-describe('blazeverify.account()', function () {
+describe('blazeverify.account()', () => {
 
-	it('should return account information', function (done) {
-		blazeverify.account().then(function (response) {
+	it('should return account information', (done) => {
+		blazeverify.account().then((response) => {
 			expect(response.owner_email).to.not.be.a('null');
 			expect(response.available_credits).to.not.be.a('null');
 			done();
