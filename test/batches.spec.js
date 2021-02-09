26e812700adb4bb6272fdddcadfbd14766f3d0e9
Jarrett Lusso <jclusso@gmail.com>
2021-02-08 21:05:19 -0500

    Changed the package to send batch requests with body data instead of URL params. Also added setTimeout to tests to stop it from hitting rate-limits

--- test/batches.spec.js
+++ test/batches.spec.js
@@ -7,10 +7,12 @@ const emails = ['deliverable@example.com', 'undeliverable@example.com']
 describe('blazeverify.batches.verify()', () => {
 
   it('should create a new batch', done => {
-    blazeverify.batches.verify(emails).then(response => {
-      expect(response.id).to.have.lengthOf(24);
-      done();
-    });
+    setTimeout(function() {
+      blazeverify.batches.verify(emails).then(response => {
+        expect(response.id).to.have.lengthOf(24);
+        done();
+      });
+    }, 1000)
   });
 
 });
@@ -18,15 +20,17 @@ describe('blazeverify.batches.verify()', () => {
 describe('blazeverify.batches.status()', () => {
 
   it('should return the status of a batch', done => {
-    blazeverify.batches.verify(emails).then(response => {
-      blazeverify.batches.status(response.id).then(response => {
-        expect(response.emails).to.not.be.a('null');
-        expect(response.total_counts).to.not.be.a('null');
-        expect(response.reason_counts).to.not.be.a('null');
-        expect(response.message).to.not.be.a('null');
-        done();
+    setTimeout(function() {
+      blazeverify.batches.verify(emails).then(response => {
+        blazeverify.batches.status(response.id).then(response => {
+          expect(response.emails).to.not.be.a('null');
+          expect(response.total_counts).to.not.be.a('null');
+          expect(response.reason_counts).to.not.be.a('null');
+          expect(response.message).to.not.be.a('null');
+          done();
+        });
       });
-    });
+    }, 1000)
   });
 
 });
